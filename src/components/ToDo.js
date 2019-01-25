cbb78d585815da4f13a19fbbb7dd3d5e224897cf
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-01-24 20:13:57 -0800

    events completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,11 +4,16 @@ class ToDo extends Component {
   render(){
     return(
       <li>
+
         <input type="checkbox" checked={this.props.isCompleted} onChange ={this.props.toggleComplete}/>
+
         <span>{this.props.description}</span>
+
+        {/* delete button with eventListener 'onClick' assigned to props.deleted (which is the anonymous function that calls for the deleteToDo method )*/}
         <button onClick={this.props.deleted}>Delete</button>
       </li>
     );
+
   }
 }
 
